8f47fb52e8de86d8899db9fa11b67befa4916920
Schirmer <schirmer>
2020-05-04 20:21:25 -0400

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello. 
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>BYe. Again.
         <a
           className="App-link"
           href="https://reactjs.org"
